e63878a4427c50f5f5613608521d17ca46eacae1
antongiannis <anton.ioannis.phys@gmail.com>
2019-06-12 02:03:39 +0100

    Included download url

--- phdTools/setup.py
+++ phdTools/setup.py
@@ -1,21 +1,31 @@
-import setuptools
+try:
+    import setuptools
+except ImportError:
+    print("Install setuptools library")
+
+install_requires = [
+    'matplotlib>=1.4.3',
+]
 
 with open(".../README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="phdTools",
-    version="0.0.1",
-    author="Ioannis Antonopoulos",
-    author_email="anton.ioannis.phys@gmail.com",
-    description="A collection of tools during PhD",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+# packages = []
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="phdTools",
+        version="0.0.1",
+        author="Ioannis Antonopoulos",
+        author_email="anton.ioannis.phys@gmail.com",
+        description="A collection of tools during PhD",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        download_url="https://github.com/antongiannis/phd_tools/",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+    )
